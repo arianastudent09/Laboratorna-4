1fa1f34da23e505c91da95d70893fc0d6e01f237
arianastudent09 <A.Voloshyna.FIT.121.20@knute.edu.ua>
2021-10-15 22:17:44 +0300

    first commit

---
+++ task3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {Console.OutputEncoding = Console.InputEncoding = System.Text.Encoding.Unicode;
+            Student st = new Student("Петров В.І", 3, "85");
+            Console.WriteLine($"Про студента: {st.Name},{st.Course},{st.Grade}");
+            Teacher asp1 = new Teacher(st.Name, st.Course, st.Grade, "Javascript");
+            Console.WriteLine($"Для викладача: {asp1.Name},{asp1.Course},{asp1.Grade},{asp1.Topic}");
+            Console.ReadLine();}
+        public class Student
+        {   protected string name;
+            protected int course; 
+            protected string grade;
+            public Student(string Name, int course, string grade)
+            {
+                this.Name = Name;
+                this.course = course;
+                this.grade = grade;
+            }
+            public string Name
+            {
+                get 
+                { 
+                    return name; 
+                }
+                set 
+                { 
+                    name = value; 
+                }
+            }
+            public int Course
+            {
+                get 
+                { 
+                    return course; 
+                }
+                set 
+                { 
+                    course = value; 
+                }
+            }
+            public string Grade
+            {
+                get 
+                { 
+                    return grade; 
+                }
+                set 
+                { 
+                    grade = value; 
+                }
+            }
+        }
+        public class Teacher : Student
+        {
+            protected string topic;            
+            public Teacher(string name, int course, string grade, string topic) :
+                base(name, course, grade)
+            {
+                
+                base.name = name; 
+                this.course = course; 
+                this.grade = grade;
+                this.topic = topic; 
+            } 
+            public string Topic
+            {
+                get 
+                { 
+                    return topic; 
+                }
+                set 
+                { 
+                    topic = value; 
+                }
+            }
+
+           
+            
+        }
+    }
+}
---
+++ task4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+         Console.OutputEncoding = Console.InputEncoding = System.Text.Encoding.Unicode;
+         Console.WriteLine("Треба ввести номер ключа доступа (1 - Trial версія, 2 - Pro версія)");
+         Console.WriteLine("Отримання Common версії при неправильному введені чи не введені номеру ключа доступу");
+         string kod = Console.ReadLine();
+         ApplicationLicense dos = new ApplicationLicense(kod);
+
+        }
+    }
+    class ApplicationLicense
+    {
+        private static void AllowTrial()
+        {
+            Console.WriteLine("Trial версія");
+        }
+        private static void AllowCommon()
+        {
+            Console.WriteLine("Common версия");
+        }
+        private static void AllowPro()
+        {
+            Console.WriteLine("Pro версия");
+        }
+        public ApplicationLicense(string kod)
+        {
+            if (kod == "1")
+            {
+                AllowTrial();
+            }
+            else if (kod == "2")
+            {
+                AllowPro();
+            }
+            else
+            {
+                AllowCommon();
+            }
+        }
+    }
+
+}
